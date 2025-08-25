8da53d0e479e13ab4213534c69bf3c0483ffc27b
gbeatrice24 <gligorbeatrice@icloud.com>
2025-08-25 19:16:51 +0300

    T1: created home page

---
+++ src/components/MainLayout.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="min-h-screen bg-pastelgreen flex flex-col items-center  text-white space-y-15">
+        <h1 class="text-[150px] font-bold pt-20">lura.</h1>
+        <h3 class="text-[36px] italic font-light">what would you say if no one knew it was you?</h3>
+        <button class="w-[322px] h-[75px] bg-white text-darkgreen1 text-[28px] font-semibold rounded-full">write a
+            confession</button>
+        <div class="flex flex-col items-center justify-center mt-auto pb-30 space-y-7">
+            <h3 class=" text-[32px] italic font-light">see todays confessions.</h3>
+            <button>
+                <img src="../assets/ArrowDown.svg" />
+            </button>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts"></script>
